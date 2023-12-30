f71e46e0b72051960c5d3a20f936147556a3277d
Arepallysindhu <sindhuarepally14@gmail.com>
2023-12-30 22:53:33 +0530

    clone

---
+++ src/Things/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "42b07407d3mshffeb5f4d724360ep10b9dejsn4264c05a086c",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
